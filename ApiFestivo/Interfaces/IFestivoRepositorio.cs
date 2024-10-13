30952cb0d1dad914830473de32171b70b1551c6e
Darwin <darwin.marin906@pascualbravo.edu.co>
2024-10-13 16:20:54 -0500

    Mi primer commit

---
+++ ApiFestivo/Interfaces/IFestivoRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using ApiFestivo.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFestivo.Core.Interfaces
+{
+    public interface IFestivoRepositorio
+    {
+        Task<IEnumerable<Festivo>> ObtenerTodosLosFestivos();
+
+        Task<Festivo> Obtener(int Id);
+
+        Task<bool> EsFestivo(Festivo fecha);
+
+        Task<Festivo> AgregarFestivo(Festivo fechaFestivo);
+
+        Task<bool> EliminarFestivo(Festivo fechaFestivo);
+
+        Task<Festivo> ActualizarFestivo(Festivo fechaFestivo);
+    }
+}
---
+++ ApiFestivo/Servicios/IFestivoServicio.cs
@@ -0,0 +1,24 @@
+﻿using ApiFestivo.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFestivo.Core.Servicios
+{
+    public interface IFestivoServicio
+    {
+        Task<IEnumerable<DateTime>> ObtenerTodosLosFestivos();
+
+        Task<Festivo> Obtener(int Id);
+
+        Task<bool> EsFestivo(DateTime fecha);
+
+        Task<DateTime> AgregarFestivo(DateTime fechaFestivo);
+
+        Task<bool> EliminarFestivo(DateTime fechaFestivo);
+
+        Task<DateTime> ActualizarFestivo(DateTime fechaFestivo);
+    }
+}
